aa89eb4803e4dcf3804bdf43a82763209c3a0792
aidandeb <132536077+aidandeb@users.noreply.github.com>
2023-09-18 14:12:44 +1000

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ window.onload = function() {
         "Continuous Delivery (CD)",
         "Infrastructure as Code (IaC)",
         "Microservices",
-        "Containerization",
+        "Containerisation",
         "DevSecOps",
         "Agile",
         "Configuration Management",
@@ -15,16 +15,29 @@ window.onload = function() {
     ];
 
     var descriptions = [
-        "The combination of cultural philosophies, practices, and tools that enhance an organization's ability to deliver applications and services at a high velocity, improving software development and infrastructure management processes.",
-        "The practice of frequently integrating code changes into a shared repository, allowing for early detection of integration issues.",
-        "An approach that ensures software is always in a releasable state by automating build, test, and deployment processes.",
-        "The practice of managing and provisioning infrastructure resources using machine-readable configuration files, enabling consistent and reproducible infrastructure setups.",
-        "An architectural approach where applications are built as a collection of small, independently deployable services that work together.",
-        "The technique of encapsulating an application and its dependencies into a lightweight, isolated container for consistent deployment across different environments.",
-        "The integration of security practices into the DevOps workflow, ensuring security is considered throughout the software development and deployment process.",
-        "An iterative and collaborative software development approach that emphasizes flexibility, customer collaboration, and responding to change.",
-        "The process of managing and maintaining the configuration settings and parameters of software systems and infrastructure components.",
-        "The use of remote servers and virtualization technologies to store, manage, and process data, providing on-demand access to computing resources."
+        "DevOps is a combination of cultural beliefs, practices, and tools that boost an organisation's ability to deliver applications and services quickly. It improves the processes of software development and infrastructure management.",
+        "Continuous Integration (CI) is the practice of frequently merging code changes into a shared repository. It helps detect integration issues early in the development process.",
+        "Continuous Delivery (CD) ensures that software is always ready for release by automating build, testing, and deployment processes.",
+        "Infrastructure as Code (IaC) involves managing and provisioning infrastructure resources using machine-readable configuration files. This ensures consistent and reproducible infrastructure setups.",
+        "Microservices is an architectural approach where applications are constructed as a collection of small, independently deployable services that work together.",
+        "Containerisation is a technique that isolates applications and their dependencies within lightweight containers. It ensures consistent deployment across various environments.",
+        "DevSecOps integrates security practices into the DevOps workflow, ensuring that security is considered throughout the entire software development and deployment process.",
+        "Agile is an iterative and collaborative software development approach that emphasises flexibility, customer collaboration, and adaptability to change.",
+        "Configuration Management involves managing and maintaining the configuration settings and parameters of software systems and infrastructure components.",
+        "Cloud Computing utilises remote servers and virtualization technologies to store, manage, and process data. It offers on-demand access to computing resources."
+    ];
+
+    var references = [
+        "https://www.techtarget.com/searchitoperations/definition/DevOps",
+        "https://aws.amazon.com/devops/continuous-integration/",
+        "https://www.thoughtworks.com/insights/blog/case-continuous-delivery",
+        "https://powershellmagazine.com/2016/01/05/devops-infrastructure-as-code-and-powershell-dsc-the-introduction/",
+        "https://www.ionos.com/digitalguide/websites/web-development/microservice-architecture/",
+        "https://www.redhat.com/en/topics/cloud-native-apps/what-is-containerization",
+        "https://aws.amazon.com/what-is/devsecops/e",
+        "https://www.agilealliance.org/agile101/",
+        "https://www.atlassian.com/microservices/microservices-architecture/configuration-management",
+        "https://azure.microsoft.com/en-us/resources/cloud-computing-dictionary/what-is-cloud-computing"
     ];
 
     for (var i = 0; i < terms.length; i++) {
@@ -35,10 +48,10 @@ window.onload = function() {
         var cell3 = document.createElement("td");
         var cell4 = document.createElement("td");
 
-        cell1.textContent = i + 1; // Display row number in column 1
-        cell2.textContent = terms[i]; // Populate words/terms in column 2
-        cell3.textContent = descriptions[i]; // Populate descriptions in column 3
-        cell4.textContent = "Row " + (i + 1) + " Col 4";
+        cell1.textContent = i + 1; 
+        cell2.textContent = terms[i]; 
+        cell3.textContent = descriptions[i]; 
+        cell4.textContent = references[i];
         
         row.appendChild(cell1);
         row.appendChild(cell2);
